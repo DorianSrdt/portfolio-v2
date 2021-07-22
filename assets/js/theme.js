102e27233aa316a2c52a0c0f77f7991717320d79
DorianSrdt <dorian.sacerdote@gmail.com>
2021-07-22 10:13:42 +0200

    first commit

---
+++ assets/js/theme.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.datepicker').forEach(function(field) {
+	var picker = new Pikaday({
+		field: field
+	});
+});
+
+/*====================== DARK LIGHT THEME =====================*/
